503e856e68ddb910b3966aa104700f35dc85d032
emmadev <macanthonyemmanuel12@gmail.com>
2023-10-18 02:56:39 +0100

    added export to csv functionality for admins

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from .models import OrderItem
-from .forms import OrderCreateForm
+# import weasyprint
 from cart.cart import Cart
 from .tasks import order_created
-
-# Create your views here.
+from .forms import OrderCreateForm
+from .models import OrderItem, Order
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
 def order_create(request):
@@ -15,23 +18,43 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(
-                    order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
             # clear the cart
             cart.clear()
-            # launch asynchronous tasks
+            # launch asynchronous task
             order_created.delay(order.id)
-
-            # set the order id in session
+            # set the order in the session
             request.session['order_id'] = order.id
-
             # redirect for payment
             return redirect(reverse('payment:process'))
-
     else:
         form = OrderCreateForm()
-
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
+
+
+"""
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
+"""
